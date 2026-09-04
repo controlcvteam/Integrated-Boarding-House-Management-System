64da624c77c74d713a60557633df40da62c78422
Jan Marinelle <janmarinellea@gmail.com>
2026-09-04 17:53:36 +0800

    Fix typo in variant type definition

--- resources/js/components/app-content.tsx
+++ resources/js/components/app-content.tsx
@@ -2,7 +2,7 @@ import { SidebarInset } from '@/components/ui/sidebar';
 import * as React from 'react';
 
 interface AppContentProps extends React.ComponentProps<'div'> {
-    variant?: 'header' | 'sidebar';
+    variant?: 'header' | 'idebar';
 }
 
 export function AppContent({ variant = 'header', children, ...props }: AppContentProps) {
